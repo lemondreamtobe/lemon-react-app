e723973657cad79d3b03f2693fd56ce7eda43553
张林锋 <897418436@qq.com>
2022-05-28 11:45:20 +0800

    add types

--- src/react-app.d.ts
+++ src/react-app.d.ts
@@ -11,14 +11,26 @@ declare module '*.avif' {
   export default src;
 }
 
-declare module '*.svg' {
+declare module '*.module.svg' {
   import * as React from 'react';
 
   export const ReactComponent: React.FunctionComponent<React.SVGProps<
     SVGSVGElement
   > & { title?: string }>;
 
+  export default ReactComponent;
+}
+declare module '*.path.svg' {
+
   const src: string;
+
+  export default src;
+}
+
+declare module '*.svg' {
+
+  const src: string;
+
   export default src;
 }
 
